60e5f013beb259aae3b78f26a50dd800b37d0c6d
Soumyanil Biswas <soumyanilbiswas2018@gmail.com>
2022-03-17 15:44:20 +0530

    update-0.1

--- user_src/client_usermode.c
+++ user_src/client_usermode.c
@@ -8,7 +8,7 @@
 #include<sys/ioctl.h>	/* ioctl() */ 
  
 #define WR_VALUE _IOW('a','a',int32_t*)
-#define RD_VALUE _IOR('a','b',int32_t*)
+//#define RD_VALUE _IOR('a','b',int32_t*)
 
 // For storing in array
 #define MAX_LIMIT 20
